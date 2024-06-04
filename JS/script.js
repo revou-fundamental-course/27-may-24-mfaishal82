c35086c05a20cfd3a0aa55273417c2dcd8fcbd19
Muhammad Faisal <mfaisal@students.hacktiv8.ac.id>
2024-06-04 08:15:35 +0700

    feat: javascript input/replace name

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,14 @@
 // javascript code
+
+function replaceName() {
+    let name = prompt("Masukkan nama anda terlebih dahulu", "")
+
+    if(!name) {
+        name = "Guest"
+    }
+
+    document.getElementById("name-guest").innerText = name
+}
+
+replaceName()
+
